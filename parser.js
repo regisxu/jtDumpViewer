5e616302ba8528074737893a224fe928e881cc53
Regis <ruizxu@ebay.com>
2014-01-06 16:35:10 +0800

    Remove old implementation.

--- parser.js
+++ parser.js
@@ -1,102 +1,232 @@
-
-function parse(dump) {
+function htmlize(dump) {
 	var lines = dump.split("\n");
 	var context = {};
-
-	context.threads = [];
+	var html = "";
 	context.parser = parseHeader;
-
+	context.html = "";
 	for (var i = 0; i < lines.length; ++i) {
-//		console.log(lines[i]);
-		context.parser = context.parser(context, lines[i]);
+		context.parser = context.parser(context, trim(lines[i]));
+		html += p(null, convertHeadingSpace(context.html)) + "\n";
 	}
 
-	return context.threads;
+	return html;
+}
+
+function trim(line) {
+	var count = 0;
+	for (var i = line.length - 1; i >= 0; --i) {
+		if (line[i] == "\r") {
+			count += 1;
+		}
+	}
+	return line.substr(0, line.length - count);
+}
+
+function convertHeadingSpace(str) {
+	var result = "";
+	for (var i = 0; i < str.length; ++i) {
+		if (str[i] == " ") {
+			result += "&nbsp;"
+		} else if (str[i] == "\t") {
+			result += "&nbsp;&nbsp;&nbsp;&nbsp;";
+		} else {
+			result += str.substring(i, str.length);
+			break;
+		}
+	}
+	return result;
+}
+
+function parseHeader(context, line) {
+	if (!isHeader(line)) {
+		context.html = line;
+		return parseHeader;
+	}	
+	var htmlize = htmlizer(line);
+	context.html = htmlize.between("\"").span("tname").split(" ", function(value) {
+		if (value == "daemon") {
+			return span("daemon", value);
+		}
+		if (value.indexOf("=") != -1) {
+			var values = value.split("=");
+			return values[0] + "=" + span(values[0], values[1]);
+		}
+		if (value.indexOf("[") == 0) {
+			return value[0] + span("oid", value.substr(1, value.length - 2)) + value[value.length - 1];
+		}
+		return value;
+	}).result;
+	return parseState;
+}
+
+function parseState(context, line) {
+	if (line.trim() == "") {
+		context.html = line;
+		return parseHeader;
+	}
+	var htmlize = htmlizer(line);
+	if (line.indexOf("(") != -1) {
+		context.html = htmlize.until(":").text().between(" ").span("state").end().text().result;
+	} else {
+		context.html = htmlize.until(": ").text().end().span("state").result;
+	}
+	return parseTrace;
+}
+
+function parseTrace(context, line) {
+	if (line.trim() == "") {
+		context.html = line;
+		return parseLocks;
+	}
+
+	if (line.indexOf("at ") != -1) {
+		var htmlize = htmlizer(line);
+		context.html = htmlize
+			.until("at ").text()
+			.until("(").span("method")
+			.until(")").span("mdesc")
+			.end().text()
+			.result;
+	} else if (line.indexOf("- None") != -1) {
+		context.html = line;
+	} else if (line.trim().indexOf("- ") == 0) {
+		var htmlize = htmlizer(line);
+		context.html = htmlize
+			.between("<", ">").span("oid")
+			.until("(a ").text()
+			.until(")").span("class")
+			.end().text()
+			.result;
+	} else {
+		context.html = line;
+	}
+
+	return parseTrace;
+}
+
+function parseLocks(context, line) {
+	context.html = line;
+	return parseLock;
+}
+
+function parseLock(context, line) {
+	if (line.trim() == "") {
+		context.html = line;
+		return parseHeader;
+	}
+	if (line.indexOf("- None") != -1) {
+		context.html = line;
+		return parseLock;
+	}
+
+	var htmlize = htmlizer(line);
+	context.html = htmlize
+		.until("- ").text()
+		.between("<", ">").span("oid")
+		.until("(a ").text()
+		.until(")").span("class")
+		.end().text()
+		.result;
+
+	return parseLock;
 }
 
 function isHeader(line) {
 	return line.trim()[0] == "\"";
 }
 
-function parseHeader(context, line) {
-	if (!isHeader(line)) {
-		return parseHeader;
+
+function span(key, value) {
+	return element("span", key, value);
+}
+
+function p(key, value) {
+	return element("p", key, value);
+}
+
+function element(element, key, value) {
+	if (key == null) {
+		return "<" + element + ">" + value + "</" + element + ">";
 	}
+	return "<" + element + " class=\"" + key + "\">" + value + "</" + element + ">";
+}
 
-	var thread = {};
-	var name = line.match("\".*\"")[0];
-	thread["name"] = name.substr(1, name.length - 2);
-	line = line.replace(name, "").trim();
+function htmlizer(dump) {
+	var htmlize = {};
+	htmlize.token = "";
+	htmlize.result = "";
+	htmlize.index = 0;
 
-	var values = line.split(" ");
-	var index = 0;
-
-	if ("daemon" == values[index]) {
-		thread["daemon"] = true;
-		index++;
-	} else {
-		thread["daemon"] = false;
-	}
-	
-	for (; index < values.length; ++index) {
-		if (values[index].indexOf("=") != -1) {
-			var kv = values[index].split("=");
-			thread[kv[0]] = kv[1];
-		} else if (values[index][0] == "[") {
-			thread["id"] = values[index].substr(1, values[index].length - 2);
+	htmlize.between = function(start, end, include) {
+		if (end == null || end == "") {
+			end = start;
 		}
+		if (include == null) {
+			include = false;
+		}
+		return htmlize.until(start, include).text().until(end, include);
+	};
+
+	htmlize.end = function() {
+		htmlize.token = dump.substring(htmlize.index, dump.length);
+		htmlize.index = dump.length;
+		htmlize.delimiter = "";
+		return htmlize;
 	}
 
-	context.threads.push(thread);
-	return parseState;
+	htmlize.until = function(delimiter, include) {
+		if (include == null) {
+			include = false;
+		}
+		var i = dump.indexOf(delimiter, htmlize.index);
+		if (i == -1) {
+			htmlize.token = "";
+			return htmlize;
+		}
+
+		if (include) {
+			i += delimiter.length;
+			htmlize.token = dump.substring(htmlize.index, i);
+			htmlize.index = i;
+			htmlize.delimiter = "";
+		} else {
+			htmlize.token = dump.substring(htmlize.index, i);
+			htmlize.index = i + delimiter.length;
+			htmlize.delimiter = delimiter;
+		}
+		return htmlize;
+	};
+
+	htmlize.split = function(separator, f) {
+		var i = -1;
+		while (htmlize.index != i) {
+			i = htmlize.index;
+			htmlize.until(separator).htmlize(f);
+		}
+		return htmlize.end().htmlize(f);
+	}
+
+	htmlize.text = function() {
+		htmlize.result += htmlize.token + htmlize.delimiter;
+		return htmlize;
+	}
+
+	htmlize.p = function(key) {
+		return htmlize.htmlize(function (value) {
+			return p(key, value);
+		});
+	}
+
+	htmlize.span = function(key) {
+		return htmlize.htmlize(function (value) {
+			return span(key, value);
+		});
+	}
+
+	htmlize.htmlize = function(f) {
+		htmlize.result += f(htmlize.token) + htmlize.delimiter;
+		return htmlize;
+	}
+
+	return htmlize;
 }
-
-function parseState(context, line) {
-	if (line.trim() == "") {
-		return parseHeader;
-	}
-
-	var values = line.split(":")[1].trim().split("(");
-	var state = {};
-	state.state = values[0].trim();
-	if (values.length == 2) {
-		state.desc = values[1].substr(0, values[1].length - 1).trim();
-	}
-	var thread = context.threads[context.threads.length - 1];
-	thread.state = state;
-	thread.stacks = [];
-	return parseTrace;
-}
-
-function parseTrace(context, line) {
-	if (line.trim() == "") {
-		return parseLocks;
-	}
-	var thread = context.threads[context.threads.length - 1];
-	thread.stacks.push(line.trim());
-	return parseTrace;
-}
-
-function parseLocks(context, line) {
-	var thread = context.threads[context.threads.length - 1];
-	thread.locks = [];
-	return parseLock;
-}
-
-function parseLock(context, line) {
-	if (line.trim() == "") {
-		return parseHeader;
-	}
-
-	var lock = {};
-	var id = line.match("<.*>");
-	if (id != null) {
-		lock.id = id[0].substr(1, id[0].length - 2);
-		var type = line.match(/\(.*\)/)[0].split(" ")[1];
-		lock.type = type.substr(0, type.length - 1);
-		var thread = context.threads[context.threads.length - 1];
-		thread.locks.push(lock);
-	}
-	return parseLock;
-}
-
--- viewer.js
+++ viewer.js
@@ -1,46 +1,178 @@
-var threads = parse(document.getElementsByTagName("p")[0].textContent);
-var list = document.getElementById("list");
-for (var i = 0; i < threads.length; ++i) {
-	var thread = threads[i];
-    var te = document.createElement("div");
-	if (thread.id != null) {
-		te.setAttribute("id", thread.id);
-	}
-	var name = document.createElement("span");
-	name.setAttribute("class", "tname");
-	name.setAttribute("name", thread.id);
-	name.setAttribute("onclick", "toggleHidden(\"" + thread.id + "\")");
-	te.appendChild(name);
-	list.appendChild(te);
-    var text = document.createTextNode(threads[i].name);
-	name.appendChild(text);
-	var stacks = document.createElement("div");
-	stacks.setAttribute("class", "stacks");
-	stacks.style.display = "none";
-	te.appendChild(stacks);
-	if (thread.stacks != null) {
-		for (var j = 0; j < thread.stacks.length; ++j) {
-			var stack = document.createElement("p");
-			var value = document.createTextNode(thread.stacks[j]);
-			stack.appendChild(value);
-			stacks.appendChild(stack);
-		}
-	}
-	te.appendChild(document.createElement("br"));
-	te.appendChild(document.createElement("br"));
+var types = ["oid", "tname", "daemon", "prio", "tid", "nid", "state", "method", "mdesc", "class"];
+var searchable = ["oid", "daemon", "prio", "state", "method", "class"];
+
+function paint(dump) {
+    var html = htmlize(dump);
+    var list = document.createElement("div");
+    list.innerHTML = html;
+
+    var ps = list.getElementsByTagName("p");
+    var thread;
+    var stacks;
+    var isStack = false;
+    var nl = document.createElement("div");
+    nl.setAttribute("id", "dump");
+
+    for (var i = 0; i < ps.length; ++i) {
+        if (isThreadHeader(ps[i])) {
+            thread = document.createElement("div");
+            thread.setAttribute("id", oid(ps[i]));
+            thread.setAttribute("class", "thread");
+            var tn = ps[i].cloneNode();
+            thread.appendChild(tn);
+            nl.appendChild(thread);
+        } else if (isState(ps[i])) {
+            stacks = document.createElement("div");
+            stacks.setAttribute("class", "stacks");
+            thread.appendChild(stacks);
+
+            stacks.appendChild(ps[i].cloneNode());
+            isStack = true;
+        } else if (isStack) {
+            stacks.appendChild(ps[i].cloneNode());
+        } else {
+            nl.appendChild(ps[i].cloneNode());
+        }
+    }
+
+    if (document.getElementById("dump") != null) {
+        document.body.removeChild(document.getElementById("dump"));
+    }    
+
+    document.body.appendChild(nl);
+
+    var color = d3.scale.category10().domain(types);
+
+    d3.selectAll("#dump span").style("color",
+                                     function(d) {
+                                         return color(this.getAttribute("class"));
+                                     })
+                              .style("cursor",
+                                     function(d) {
+                                         return isSearchable(searchable, this.getAttribute("class")) ? "pointer" : null;
+                                     })
+                              .attr("onclick",
+                                    function(d) {
+                                        return isSearchable(searchable, this.getAttribute("class")) ? "searchSpan(this)" : null;
+                                    });
+
+    d3.selectAll("#dump span.tname").attr("onclick", "toggleHidden(this)");
 }
 
-function toggleHidden(id) {
-	var parent = document.getElementById(id);
-	for (var i = 0; i < parent.childNodes.length; ++i) {
-		var child = parent.childNodes[i];
-		if (child.getAttribute("class") == "stacks") {
-			if (child.style.display == "none") {
-				child.style.display = "";
-			} else {
-				child.style.display = "none";
-			}
-			return;
-		}
-	}
+function isSearchable(searchable, type) {
+    return ploop(searchable, function(d) { return d == type; });
+}
+
+function isThreadHeader(p) {
+    return p.querySelector("span.tname") != null;
+}
+
+function oid(p) {
+    var span = p.querySelector("span.oid");
+    if (span != null) {
+        return span.textContent;
+    }
+    return "";
+}
+
+function isState(p) {
+    return p.querySelector("span.state") != null;
+}
+
+function toggleHidden(n) {
+    var parent = n.parentNode.parentNode;
+    var stacks = parent.querySelector(".stacks");
+    if (stacks != null) {
+        stacks.style.display == "none" ? stacks.style.display = "" : stacks.style.display = "none";
+    }
+}
+
+document.getElementById("fpath").addEventListener("keypress",
+                                                  function(event) {
+                                                      if(event.keyCode != 13) {
+                                                          return;
+                                                      }
+                                                      paint(readFile(event));
+                                                      document.getElementById("tdump").value = "";
+                                                  },
+                                                  false);
+
+document.getElementById("tdump").addEventListener("keypress",
+                                                  function(event) {
+                                                      if(event.keyCode != 13) {
+                                                          return;
+                                                      }
+                                                      paint(event.target.value);
+                                                      document.getElementById("fpath").value = "";
+                                                  },
+                                                  false);
+
+document.getElementById("search").addEventListener("keyup", function(event) { searchText(this.value); });
+
+function search(txt) {
+    if (txt.split(":").length == 2) {
+        var kv = txt.split(":");
+        searchPair(kv[0], kv[1]);
+    } else {
+        searchText(txt);
+    }
+}
+
+function searchText(txt) {
+    show(function(thread) {
+        return ploop(thread.querySelectorAll("p"),
+                     function(p) {
+                         return p.textContent.toLowerCase().indexOf(txt.toLowerCase()) != -1;
+                     });
+    });
+}
+
+function searchPair(type, value) {
+    show(function(thread) {
+        return ploop(thread.querySelectorAll("span." + type),
+                     function(sp) {
+                         return sp.textContent == value;
+                     });
+    });
+}
+
+function readFile(event) {
+    if (event.target.value == null || event.target.value.trim() == "") {
+        return;
+    }
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", event.target.value, false);
+    rawFile.send(null);
+    if(rawFile.status === 200 || rawFile.status == 0) {
+        return rawFile.responseText;
+    }
+}
+
+function searchSpan(span) {
+    if(span.getAttribute("class") != null) {
+        var s = document.getElementById("search");
+        s.value = span.getAttribute("class") + ":" + span.textContent;
+        search(s.value);
+    }
+}
+
+function show(select) {
+    loop(document.querySelectorAll(".thread"),
+         function(thread) { thread.style.display = (select(thread) ? "" : "none"); }
+        );
+}
+
+function loop(list, f) {
+    for (var i = 0; i < list.length; ++i) {
+        f(list[i]);
+    }
+}
+
+function ploop(list, p) {
+    for (var i = 0; i < list.length; ++i) {
+        if (p(list[i])) {
+            return true;
+        }
+    }
+    return false;
 }
